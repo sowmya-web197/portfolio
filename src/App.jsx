b892de1e36e810972602c2ce57eeec9817a61fca
Sowmya Dasari <dasarisowmya78@gmail.com>
2026-06-01 16:17:35 +0530

    app about to load

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Home from '../Home'
+import About from './About'
 
 const App = () => {
-  console.log('bug fixed')
+  console.log('app about to load')
   return (
     <div>
       <h1>Welcome to the App</h1>
       <Home />
+      <About />
     </div>
   )
 }
